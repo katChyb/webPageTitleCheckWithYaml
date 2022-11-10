e7378c10525fa60735e5fd91c3c02fefde7ab90e
katChyb <64010913+katChyb@users.noreply.github.com>
2022-11-10 19:25:19 +0100

    working linked to jenkins 10/11

--- src/test/java/WebPageTitleTest.java
+++ src/test/java/WebPageTitleTest.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class WebPageTitleCheck extends BaseTest {
-    private static Logger log = LoggerFactory.getLogger(WebPageTitleCheck.class);
+public class WebPageTitleTest extends BaseTest {
+    private static Logger log = LoggerFactory.getLogger(WebPageTitleTest.class);
     @Tag("regression")
     @DisplayName("Page title validation test")
     @Test
